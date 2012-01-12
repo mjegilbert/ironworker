25dd2e0e91b05174dcddade77d53961fd5f844dc
Mark Gilbert <mgilbert@foundryhiring.com>
2012-01-12 12:02:12 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ironworker::Application.config.secret_token = '20ce3425b4347e6b18c8a28f39b503bd7efdce1cb50a88ed153bc788e37e95f7f940d37570aaf1366900be7b0dd30586de8ac6c840afe17e8af9d5fd9f05b921'
